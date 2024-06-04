dc2b4c76f2f27fe00f94075dd8cf6b1ff6970082
hasibur14 <hasib141415@gmail.com>
2024-06-04 22:35:12 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-//const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 //const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
@@ -31,6 +31,46 @@ async function run() {
 
         const mealCollection = client.db('dineDorm').collection('meals');
         const userCollection = client.db('dineDorm').collection('users');
+
+
+        // jwt related api --generate jwt
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+            res.send({ token })
+        });
+
+        
+        //Middleware related api...
+        const verifyToken = (req, res, next) => {
+            // console.log('inside verify token', req.headers.authorization)
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: ' unauthorized access' })
+                }
+                req.decoded = decoded
+                next()
+            })
+        };
+
+
+        //user verify admin after verify token
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            console.log("67", email)
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            console.log('70', user)
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: ' forbidden access' })
+            }
+            next()
+        };
 
 
         /**
